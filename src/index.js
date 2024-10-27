bbb71b0733e3fe4925bd2e808b49bf0cc6ed9087
Rachael Alexander <rachael@inox-world.com.au>
2024-10-27 20:50:34 +1100

    Improved UX

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generateStory(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "beeet5337ba0d42b5b6310f3e39o1452";
-    let context = "You are a short story writer. Your mission is to generate a short story in basic HTML format of no more than 6 lines. Separate each line with a <br/>, but output the story without any extra symbols or code delimiters. Sign the bottom of the story with SheCodes AI in bold text";
+    let context = "You are a short story writer. Your mission is to generate a short story in basic HTML format of no more than 6 lines. Separate each line with a <br/>, but output the story without any extra symbols or code delimiters. Sign the bottom of the story with 'SheCodes AI' using a <strong> element";
     let prompt = `User instructions: Generate a short story about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let storyElement = document.querySelector("#short-story");
+    storyElement.classList.remove("hidden");
+    storyElement.innerHTML = `<div class="generating">Generating short story about ${instructionsInput.value} </div>`
 
     console.log("Generating story...");
     console.log(`Prompt: ${prompt}`);
